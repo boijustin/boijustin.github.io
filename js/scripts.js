ed1af69851c283c4ff09e9c085c8c913ab4d3225
Justin Chew <Justin Chew>
2016-06-17 16:40:05 -0700

    try

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+﻿function isMobile() {
+    return (/Android|iPhone|iPad|iPod|Blackberry/i).text(navigator.userAgent || navigator.vendor || window.opera);
+}
+
+if (!isMobile()) {
+    jQuery(document).ready(function ($) {
+        $(window).scroll(function () {
+            var scroll = $(window).scrollTop(), slowScroll = scroll / 3;
+            var headerheight = $('section').height();
+            $('section').css({ transform: "translateY(" + slowScroll + "px)" });
+            $('section .container').css({ opacity: Math.min(1, 1.2 - scroll / headerheight) });
+        });
+    });
+}
