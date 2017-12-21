6b6dbdcf001616445c808db3eef6a4dc5fae04b2
boijustin <justin.chew@gmail.com>
2017-12-20 23:24:17 -0800

    added google maps api

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,32 @@ if (!isMobile.any()) {
     document.getElementById("wall2").style.backgroundAttachment = "fixed";
 
 };
+
+
+
+function initialize() {
+    var mapOptions = {
+        center: new google.maps.LatLng(0, 0),
+        zoom: 4
+    };
+    var mapOptions = { center: new google.maps.LatLng(47.518128, -122.148743), zoom: 9 };
+    var map = new google.maps.Map(document.getElementById("MapDiv"), mapOptions);
+    addButtons(map);
+}
+
+function addButtons(map) {
+    document.getElementById('btnTerrain').addEventListener('click', function () {
+        map.setMapTypeId(google.maps.MapTypeId.TERRAIN);
+    });
+    document.getElementById('btnRoadmap').addEventListener('click', function () {
+        map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
+    });
+    document.getElementById('btnHybrid').addEventListener('click', function () {
+        map.setMapTypeId(google.maps.MapTypeId.HYBRID);
+    });
+    document.getElementById('btnSatellite').addEventListener('click', function () {
+        map.setMapTypeId(google.maps.MapTypeId.SATELLITE);
+    });
+}
+
+google.maps.event.addDomListener(window, "load", initialize);
